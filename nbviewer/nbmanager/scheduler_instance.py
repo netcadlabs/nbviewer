9cce06c37818b6c4da14ec62304bdc7e43f77136
agit oktay <agitoktay@gmail.com>
2021-04-19 15:42:57 +0300

    remove CronTabsSchedulerProvider

--- nbviewer/nbmanager/scheduler_instance.py
+++ nbviewer/nbmanager/scheduler_instance.py
@@ -1,6 +1,5 @@
 import os
 
-from nbviewer.nbmanager.scheduler.cron_tabs_scheduler_provider import CronTabsSchedulerProvider
 from nbviewer.nbmanager.scheduler.quartz_scheduler_provider import QuartzSchedulerProvider
 from nbviewer.nbmanager.scheduler.schedule_module_scheduler_provider import ScheduleModuleSchedulerProvider
 
@@ -11,12 +10,10 @@ class SchedulerInstance:
     @staticmethod
     def get(log: any = None) -> 'SchedulerProvider':
         if SchedulerInstance.__instance is None:
-
             SCHEDULER_TYPE = os.getenv('SCHEDULER_TYPE', 'scheduler')
-
-            if SCHEDULER_TYPE == 'crontab':
-                SchedulerInstance.__instance = CronTabsSchedulerProvider(log=log, user=os.getenv('CRON_USERNAME', os.getlogin()))
-            elif SCHEDULER_TYPE == 'quartz':
+            # if SCHEDULER_TYPE == 'crontab':
+                # SchedulerInstance.__instance = CronTabsSchedulerProvider(log=log, user=os.getenv('CRON_USERNAME', os.getlogin()))
+            if SCHEDULER_TYPE == 'quartz':
                 SchedulerInstance.__instance = QuartzSchedulerProvider(log=log, url=os.getenv('QUARTZ_SERVICE', None))
             elif SCHEDULER_TYPE == 'scheduler':
                 SchedulerInstance.__instance = ScheduleModuleSchedulerProvider(log=log)
